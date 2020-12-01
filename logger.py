232e3de61851c7c9540f5c4b5051e898022bd228
kenarvyas <kenarvyas@gmail.com>
2020-11-30 16:27:42 -0800

    loop fix

--- logger.py
+++ logger.py
@@ -7,11 +7,12 @@ def logger(track, trial):
 
     start = time.time()
     iter = 0
-    while(iter < 10):
+    while(keyval != -3):
         path = "/data/%s/%s_%03d" % (base, base, iter)
         screenshot(path);
         keyval = keystroke();
-
+        if(keyval == -3):
+            break
         f.write("%s, %3f \n" % (path, keyval))
         iter = iter + 1
 
@@ -30,13 +31,16 @@ def keystroke():
     start = time.time()
     while(elap < 1):
         elap = time.time() - start
-        # val = code for logging keystroke (1 for right, -1 for left)
+        # val = code for logging keystroke (1 for right, -1 for left, 0 if nothing, -3 if q)
+        if(val == -3):
+            return -3;
         val = 1 # delete
         sum = sum + val
         iter = iter + 1
     return sum / iter
 
 # First command line argument: track, second command line argument: trial number
+# Use q to break from program
 if __name__ == "__main__":
     track = sys.argv[1]
     trial = sys.argv[2]
