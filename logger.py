6b63b684f7e783ebf49256166605f53a607d16fe
Sahil Ambardekar <ambardekarsahil@gmail.com>
2020-12-04 02:23:17 -0500

    final data logging script

--- logger.py
+++ logger.py
@@ -7,34 +7,30 @@ from threading import Thread
 from pathlib import Path
 
 sum = 0
-iter = 0
 
 
 def logger(track, trial):
     app('sixtyforce').activate()
     time.sleep(10)
-    base = track + "_trial_" + trial
+    base = "data/data/" + track + "_trial_" + trial
 
     f = open("%s" % base, "a")
 
     start = time.time()
     iter1 = 0
     keyval = 0
-    while(keyval != -3):
-        path = "data/%s_%03d" % (base, iter1)
-        # path = "/%s_%03d" % (base, iter1)
+    while (True):
+        path = "data/screenshots/%s_%05d.png" % (base, iter1)
         screenshot(path)
         keyval = keystroke()
-        # if(keyval == -3):
-        #     break
-        print("hi")
+        if keyval == -3:
+            break
         f.write("%s, %3f \n" % (path, keyval))
-        print("another one")
         iter1 = iter1 + 1
 
     end = time.time()
     f.close()
-    print("Total time taken: %3f sec" % ((end-start)/iter))
+    print("Total time taken: %3f sec" % (end-start))
 
 
 def screenshot(path):
@@ -42,57 +38,34 @@ def screenshot(path):
     os.system(f'screencapture -R 750,245,620,450 {path}')
     print("after")
 
-
 def keystroke():
-    global iter
     global sum
     sum = 0
-    iter = 0
     with Listener(on_press=on_press) as listener:
         def time_out(period_sec: int):
             time.sleep(period_sec)
             listener.stop()
             # Setup the listener
-        Thread(target=time_out, args=(1.0,)).start()
+        Thread(target=time_out, args=(0.5,)).start()
         listener.join()  # Join the thread to the main thread
-    # elap = 0
-    # start = time.time()
-    # while(elap < 1):
-    #     elap = time.time() - start
-    #     # val = code for logging keystroke (1 for right, -1 for left, 0 if nothing, -3 if q)
-    #     if(val == -3):
-    #         return -3
-    #     val = 1 # delete
-    #     sum = sum + val
-    #     iter = iter + 1
-    # listener.stop()
-    if iter == 0:
-        iter = 1
-    return sum / iter
+    return sum / 15
 
 
 def on_press(key):
     global iter
     global sum
     val = 0
-    iter = iter + 1
     if hasattr(key, 'char'):  # Write the character pressed if available
         if key.char == 'd':
             val = 1
         elif key.char == 'a':
             val = -1
+        elif key.char == 'q':
+            val = 0
+            sum = -45
     sum = sum + val
-    # # elif key.name == 'a':
-    # #     print("a")
-    # # elif key.name == 's':
-    # #     print("s")
-    # # elif key.name == 'd':
-    # #     print("d")
-    # else:  # If anything else was pressed, write [<key_name>]
-    #     f.write('[' + key.name + ']')
 
 
-    # f.close()  # Close the file
 # First command line argument: track, second command line argument: trial number
 # Use q to break from program
 if __name__ == "__main__":
